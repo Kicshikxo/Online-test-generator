c761af09ead03eb96c3111c001964038fef71ab1
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-09 02:35:40 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [],
+	css: ['@/static/vuetify.loaded.css'],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [],
