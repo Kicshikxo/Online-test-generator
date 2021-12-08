fa2bb359ae70b6ec564870aff2e0f9391daebfd2
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-09 02:50:30 +0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,13 +13,13 @@ export default {
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-			{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css' }
+			// { rel: 'stylesheet', href: '/vuetify.loaded.css' },
 			// { rel: 'preload', href: '/vuetify.loaded.css', as: 'style' }
 		]
 	},
 
 	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: [],
+	css: [{ src: '@/static/vuetify.loaded.css', ssr: false }],
 
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [],
