290e60dc44ed1494449dc238730e4809fda568de
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-22 22:55:09 +0300

    updated login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,52 @@
 <template>
 	<div>
-		<div class='background-content'>
-			<v-container class='background-content-container'>
-				<h1 class='background-content-header my-8'>Логин</h1>
+		<div class="background-content">
+			<v-container class="background-content-container">
+				<h1 class="background-content-header my-8">Логин</h1>
 			</v-container>
 		</div>
-		<v-container class='main-content'>
+		<v-container class="main-content">
 			<v-row>
 				<v-col>
-					<v-card>
+					<v-card class="login-card">
 						<v-card-title class="headline">
-							Популярные тесты
+							Для входа заполните необходимые поля
 						</v-card-title>
 						<v-card-text>
-							<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+							<v-form ref="form" v-model="valid">
+								<v-text-field
+									solo
+									v-model="login"
+									:rules="loginRules"
+									label="Имя пользователя"
+									autocomplete="off"
+									spellcheck="false"
+									required
+								></v-text-field>
+								<v-text-field
+									solo
+									v-model="password"
+									:rules="passwordRules"
+									type="password"
+									label="Пароль"
+									autocomplete="off"
+									spellcheck="false"
+									required
+								></v-text-field>
+							</v-form>
 						</v-card-text>
+						<v-card-actions class="ma-2">
+							<v-spacer></v-spacer>
+							<v-btn
+								:disabled="!valid || loading"
+								:loading="loading"
+								@click="loginRequest"
+								color="primary"
+							>
+								Войти
+								<v-icon right>{{ mdiKey }}</v-icon>
+							</v-btn>
+						</v-card-actions>
 					</v-card>
 				</v-col>
 			</v-row>
@@ -23,16 +55,54 @@
 </template>
 
 <script>
+import { mdiKey } from "@mdi/js";
 export default {
+	data: () => ({
+		mdiKey,
+		valid: false,
+		login: "",
+		loginRules: [(v) => !!v || "Логин обязателен"],
+		password: "",
+		passwordRules: [(v) => !!v || "Пароль обязателен"],
+		loading: false,
+	}),
+	methods: {
+		async loginRequest() {
+			if (this.loading) {
+				return;
+			}
 
-}
+			this.loading = true;
+
+			const result = await this.$api("auth/login", {
+				login: this.login,
+				password: this.password,
+			});
+
+			if (result.success) {
+				this.$store.commit("auth/loggedIn", result.user);
+				this.$toast("Успешный вход", { type: "success" });
+				this.$router.push(result.redirect);
+			} else {
+				this.$toast("Ошибка входа", { type: "error" });
+				setTimeout(() => {
+					this.loading = false;
+				}, 500);
+			}
+		},
+	},
+};
 </script>
 
 <style scoped>
+.login-card {
+	height: 50vh;
+}
+
 .background-content {
 	width: 100vw;
 	height: 280px;
-	background-color: #00202D
+	background-color: #00202d;
 }
 .background-content-container {
 	max-width: 1150px;
