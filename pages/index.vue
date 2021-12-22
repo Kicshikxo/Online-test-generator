a4396b95d31a87d916f99dc72bbc46c0b5337811
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-22 22:55:59 +0300

    changed index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
 <template>
 	<div>
-		<div class='background-content'>
-			<v-container class='background-content-container'>
-				<h1 class='background-content-header my-8'>Online Test Generator</h1>
-				<p class='background-content-paragraph'>
-					Платформа для создания тестов<br/>
-					Создавать тесты теперь легко и удобно!</p>
+		<div class="background-content">
+			<v-container class="background-content-container">
+				<h1 class="background-content-header my-8">
+					Online Test Generator
+				</h1>
+				<p class="background-content-paragraph">
+					Платформа для создания тестов<br />
+					Создавать тесты теперь легко и удобно!
+				</p>
 			</v-container>
 		</div>
-		<v-container class='main-content'>
+		<v-container class="main-content">
 			<v-row>
 				<v-col>
 					<v-card>
@@ -16,7 +19,7 @@
 							Популярные тесты
 						</v-card-title>
 						<v-card-text>
-							<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+							<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 						</v-card-text>
 					</v-card>
 				</v-col>
@@ -26,17 +29,13 @@
 							Недавние тесты
 						</v-card-title>
 						<v-card-text>
-							<br><br><br><br><br><br><br><br><br><br><br>
+							<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 						</v-card-text>
 						<v-card-actions>
 							<v-spacer />
-							<v-btn color="primary" @click='api'>
-								API
-								<v-icon right>{{mdiSwapVerticalBold}}</v-icon>
-							</v-btn>
 							<v-btn color="primary" nuxt to="/inspire">
-								Continue
-								<v-icon right>{{mdiChevronRight}}</v-icon>
+								Продолжить
+								<v-icon right>{{ mdiChevronRight }}</v-icon>
 							</v-btn>
 						</v-card-actions>
 					</v-card>
@@ -46,28 +45,22 @@
 	</div>
 </template>
 <script>
-import { mdiSwapVerticalBold, mdiChevronRight } from '@mdi/js';
+import { mdiSwapVerticalBold, mdiChevronRight } from "@mdi/js";
 
 export default {
 	data: () => ({
 		mdiSwapVerticalBold,
-		mdiChevronRight
+		mdiChevronRight,
 	}),
-	methods: {
-		async api() {
-			const apiData = await this.$api('test')
-
-			this.$toast(JSON.stringify(apiData))
-		}
-	}
-}
+	methods: {},
+};
 </script>
 
 <style scoped>
 .background-content {
 	width: 100vw;
 	height: 442px;
-	background-color: #1C69B2
+	background-color: #1c69b2;
 }
 .background-content-container {
 	max-width: 1150px;
