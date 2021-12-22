a5a485e3476e4211dff61a5e3a75fd55035bbea9
Kicshikxo <supermegavalera9000@gmail.com>
2021-12-22 22:55:48 +0300

    added auth page

---
+++ pages/auth.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-container fluid>
+		<div>Если эта страница открывается, то вы авторизованы</div>
+	</v-container>
+</template>
+
+<script>
+export default {
+	middleware: "auth",
+};
+</script>
+
+<style>
+</style>
