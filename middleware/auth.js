fb67b114a4fd738e735768c57116ea78e0ec29c4
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-14 08:15:54 +0300

    fixed auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,14 @@
 export default async ({ $api, $toast, store, redirect }) => {
-	if (!store.state.auth.isLoggedIn) {
-		const result = await $api('auth')
+	const result = await $api('auth')
 
-		if (result.success) {
-			store.commit('auth/loggedIn', result.user)
-		} else {
-			if (process.client && $toast) {
-				$toast('Ошибка доступа', {
-					type: 'error'
-				})
-			}
-			redirect('/')
+	if (result.success && !store.state.auth.isLoggedIn) {
+		store.commit('auth/loggedIn', result.user)
+	} else {
+		if (process.client && $toast) {
+			$toast('Ошибка доступа', {
+				type: 'error'
+			})
 		}
+		redirect('/')
 	}
 }
