6b86aa5130878087feb1564430e4b340c5a245fd
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-22 07:52:23 +0300

    added @nuxtjs/html-minifier

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,8 @@ export default {
 		// https://go.nuxtjs.dev/axios
 		'@nuxtjs/axios',
 		// https://go.nuxtjs.dev/pwa
-		'@nuxtjs/pwa'
+		'@nuxtjs/pwa',
+		'@nuxtjs/html-minifier'
 	],
 
 	// Axios module configuration: https://go.nuxtjs.dev/config-axios
