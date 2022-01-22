e479821f588d2c5edd8386e2b81de5fd1b17dc95
Kicshikxo <supermegavalera9000@gmail.com>
2022-01-22 13:21:40 +0300

    added dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-container class="d-flex justify-center">
+		<h1>Добро пожаловать, {{ $store.state.auth.user.username }}</h1>
+	</v-container>
+</template>
+
+<script>
+export default {
+	middleware: "auth",
+};
+</script>
+
+<style>
+</style>
